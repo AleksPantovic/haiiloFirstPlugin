6270bd2e7ee550e3617ed6d5e5a986a0225dc374
AleksPantovic <aleksandar.pantovic@asioso.com>
2025-02-28 15:28:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 {
-    "manifestVersion": "2.0.0",
-    "pluginVersion": "0.1.0",
-    "name": {
-      "en": "Demo Plugin - Step 1",
-      "de": "Demo Plugin - Step 1"
+  "manifestVersion": "2.0.0",
+  "pluginVersion": "0.1.0",
+  "name": {
+    "en": "Demo Plugin - Step 1a",
+    "de": "Demo Plugin - Step 1a"
+  },
+  "description": {
+    "en": "A simple demo plug-in",
+    "de": "Ein einfaches Beispiel-Plug-in"
+  },
+  "entryPoints": [
+    {
+      "id": "510e9e0c-e3b8-44a8-8db1-5185d3d844ce",
+      "name": {
+        "en": "View 1",
+        "de": "Ansicht 1"
+      },
+      "description": {
+        "en": "Displaying view 1",
+        "de": "Ansicht 1 wird angezeigt"
+      },
+      "url": "/ep1.html"
     },
-    "description": {
-      "en": "A simple demo plug-in",
-      "de": "Ein einfaches Beispiel-Plug-in"
-    },
-    "entryPoints": [
-      {
-        "id": "510e9e0c-e3b8-44a8-8db1-5185d3d844ce",
-        "url": "/",
-        "name": {
-          "en": "My first plug-in view",
-          "de": "Meine erste Plug-in Ansicht"
-        },
-        "description": {
-          "en": "Showing my plug-in",
-          "de": "Mein Plug-in"
-        }
-      }
-    ]
-  }
+    {
+      "id": "4f434887-9289-48ac-986d-4ff1f37ab618",
+      "name": {
+        "en": "View 2",
+        "de": "Ansicht 2"
+      },
+      "description": {
+        "en": "Displaying view 2",
+        "de": "Ansicht 2 wird angezeigt"
+      },
+      "url": "/ep2.html"
+    }
+  ]
+}
